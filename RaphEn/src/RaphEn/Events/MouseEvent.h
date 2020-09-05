d5389050f8e397bf50728aa8278a1ab36dd14d16
Rapha <rhja@uol.com.br>
2020-09-05 01:19:25 -0300

    add mouse button event classes

--- RaphEn/src/RaphEn/Events/MouseEvent.h
+++ RaphEn/src/RaphEn/Events/MouseEvent.h
@@ -8,7 +8,7 @@
 namespace raphen::events
 {
 	// base class for any Mouse Events (mouse moved, click, scroll, etc)
-	class MouseEvent : Event
+	class MouseEvent : public Event
 	{
 	public:
 		std::string ToString() const override
@@ -18,20 +18,20 @@ namespace raphen::events
 			return ss.str();
 		}
 
-		inline double GetX() { return m_mouse_x; }
-		inline double GetY() { return m_mouse_y; }
+		inline double GetX() const { return m_mouse_x; }
+		inline double GetY() const { return m_mouse_y; }
 
 		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
 
 	protected:
 		MouseEvent (double x, double y)
 			: m_mouse_x(x) , m_mouse_y (y) {}
-	private:
+
 		double m_mouse_x, m_mouse_y;
 	};
 
 	//Holds just the coordinates of the movement
-	class RE_API MouseMovedEvent : MouseEvent
+	class RE_API MouseMovedEvent : public MouseEvent
 	{
 	public:
 		MouseMovedEvent(double x, double y) :
@@ -40,14 +40,15 @@ namespace raphen::events
 		EVENT_CLASS_TYPE(MouseMoved)
 	};
 
-	class RE_API MouseScrolledEvent : MouseEvent
+
+	class RE_API MouseScrolledEvent : public MouseEvent
 	{
 	public:
 		MouseScrolledEvent(double x, double y, double _x_offset, double _y_offset)
 			:MouseEvent(x,y) , x_offset(_x_offset), y_offset(_y_offset) {}
 
-		inline double GetXOffset() { return x_offset; }
-		inline double GetYOffset() { return y_offset; }
+		inline double GetXOffset() const { return x_offset; }
+		inline double GetYOffset() const { return y_offset; }
 
 		std::string ToString() const override
 		{
@@ -60,5 +61,54 @@ namespace raphen::events
 
 	private:
 		double x_offset, y_offset;
+	};
+
+
+	class RE_API MouseButtonEvent : public MouseEvent
+	{
+	public:
+		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput | EventCategoryMouseButton)
+
+		inline unsigned int GetMouseButton() const { return m_mouse_button; }
+
+	protected:
+		MouseButtonEvent (double x, double y, unsigned int button) :
+			MouseEvent(x,y), m_mouse_button(button) {}
+
+	protected:
+		unsigned int m_mouse_button;
+	};
+
+
+	class RE_API MouseButtonDownEvent : public MouseButtonEvent
+	{
+	public:
+		MouseButtonDownEvent(double x, double y, unsigned int button) :
+			MouseButtonEvent(x,y,button) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "MouseButtonDownEvent: (" << m_mouse_x << "," << m_mouse_y << "), button: " << m_mouse_button;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(MouseButtonDown)
+	};
+
+	class RE_API MouseButtonUpEvent : public MouseButtonEvent
+	{
+	public:
+		MouseButtonUpEvent(double x, double y, unsigned int button) :
+			MouseButtonEvent(x, y, button) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "MouseButtonUpEvent: (" << m_mouse_x << "," << m_mouse_y << "), button: " << m_mouse_button;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(MouseButtonUp)
 	};
 }
